023ea71165d33fb89d6c3428cf0492930fe300ae
AngelAndradeGarcia <angel.andradesb@gmail.com>
2023-03-24 12:50:55 -0600

    Ej1 C Java BASICO

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+            //1. numericos
+            //1.1 enteros
+            byte variable1 = 5;
+        System.out.println(variable1);
+            short variable2 = 10;
+        System.out.println(variable2);
+            int variable3 = 30;
+        System.out.println(variable3);
+            long variable4 = 100;
+        System.out.println(variable4);
+            //1.2 decimales
+            float variable5 = 5.5f;
+        System.out.println(variable5);
+            double variable6 = 10.5d;
+        System.out.println(variable6);
+            variable6 = 20.5d;
+        System.out.println(variable6);
+
+            //2. Booleano
+            boolean variable7 = false;
+        System.out.println(variable7);
+            boolean variable8 = true;
+        System.out.println(variable8);
+
+            //3. Texto
+            char variable9 = 'a';
+        System.out.println(variable9);
+            String variable10 = "Lorem ipsum dolor sit amet, consectetur adipiscing";
+        System.out.println(variable10);
+    }
+}
